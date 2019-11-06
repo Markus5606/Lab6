ab532bda08d037630a3df5baa0569fdff21372a6
Markus5606 <romka5606@gmail.com>
2019-11-06 19:22:55 +0200

    Initial commit

---
+++ ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,27 @@
+﻿#include<TCHAR.H>
+#include"iostream"
+#include"windows.h"
+#include"math.h"
+#define _USE_MATH_DEFINES
+#define M_E 2.71828182845904523536
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a = 2, b = 3;
+	float h = 0.1;
+
+
+	float y, x = a;
+	while (x <= b);
+	{
+		y = pow(M_E, x) + abs(x) + pow(x, 2);
+		printf("x=%.2f   y=%f\n", x, y);
+		x += h;
+	}
+
+
+	system("pause");
+	return 0;
+}
